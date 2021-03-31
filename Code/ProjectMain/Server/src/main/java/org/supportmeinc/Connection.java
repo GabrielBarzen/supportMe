8aec4f86f23aa56022642486015c53992e88f62e
GabrielModin <gabriel.barzen@mailfence.com>
2021-03-31 09:41:35 +0200

    Server connection established diagram created in code

--- Code/ProjectMain/Server/src/main/java/org/supportmeinc/Connection.java
+++ Code/ProjectMain/Server/src/main/java/org/supportmeinc/Connection.java
@@ -1,2 +1,84 @@
-package org.supportmeinc;public class Connection {
+package org.supportmeinc;
+
+import shared.User;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Connection {
+
+    private Socket socket;
+    private Send send;
+    private Receive receive;
+    private User user;
+
+
+    private ObjectReceivedListener objectReceivedListener;
+
+    ObjectInputStream inputStream;
+    ObjectOutputStream outputStream;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        this.send = new Send();
+        this.receive = new Receive();
+
+        inputStream = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+        outputStream = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+        send.start();
+        receive.start();
+    }
+
+    public void setObjectReceivedListener(ObjectReceivedListener listener){
+        this.objectReceivedListener = listener;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    private class Send extends Thread{
+
+        Buffer<Object> objectBuffer = new Buffer<>();
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()){
+                try {
+                    outputStream.writeObject(objectBuffer.get());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private class Receive extends Thread{
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()){
+                try {
+                    Object object = inputStream.readObject();
+                    if (object instanceof User){
+                        setUser((User) object);
+                    }
+                    objectReceivedListener.objectReceived(object,getUser());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
 }
--- Code/ProjectMain/Server/src/main/java/org/supportmeinc/ConnectionManager.java
+++ Code/ProjectMain/Server/src/main/java/org/supportmeinc/ConnectionManager.java
@@ -1,2 +1,65 @@
-package org.supportmeinc;public class ConnectionManager {
+package org.supportmeinc;
+
+import shared.Thumbnail;
+import shared.User;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class ConnectionManager implements Runnable, ObjectReceivedListener{
+
+    private ServerSocket serverSocket;
+    private Thread acceptConnectionThread;
+    HashMap<User,Connection> userConnection;
+
+    public ConnectionManager(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+        start();
+    }
+
+    private void start(){;
+        if (acceptConnectionThread == null) {
+            acceptConnectionThread = new Thread(this);
+        } else {
+            return;
+        }
+        acceptConnectionThread.start();
+    }
+
+    LinkedList<Connection> newConnections = new LinkedList<>();
+
+    @Override
+    public void run() {
+        while (!Thread.interrupted()){
+            try {
+                Connection connection = new Connection(serverSocket.accept());
+                connection.setObjectReceivedListener(this);
+                newConnections.add(connection);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void objectReceived(Object object, User user) {
+
+        if (object instanceof User){
+            for (Connection connection: newConnections) {
+                if(connection.getUser() == user){
+                    userConnection.put(user, connection);
+                    newConnections.remove(connection);
+                }
+            }
+        }
+
+        if (object instanceof Thumbnail){
+
+        }
+
+        if (object instanceof Thumbnail[]){
+
+        }
+    }
 }
