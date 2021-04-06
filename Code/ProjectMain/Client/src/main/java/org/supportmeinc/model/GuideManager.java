ec89064bf2383b8af99337b2c3af7fb39b4c83e7
Raion1 <isak.holmqvist01@gmail.com>
2021-04-06 10:45:55 +0200

    Added som key functionality on how to add a card on backend side

--- Code/ProjectMain/Client/src/main/java/org/supportmeinc/model/GuideManager.java
+++ Code/ProjectMain/Client/src/main/java/org/supportmeinc/model/GuideManager.java
@@ -4,12 +4,16 @@ import shared.Card;
 import shared.Guide;
 import shared.Thumbnail;
 
+import java.util.ArrayList;
+
 public class GuideManager {
 
     private Guide currentGuide;
     private Guide[] guides;
     private Thumbnail[] thumbnails; // fixa plz
     private Connection connection;
+    private Card[][] cards;
+    private ArrayList<Card> cardArrayList;
 
     public GuideManager(Connection connection) {
         this.connection = connection;
@@ -27,6 +31,30 @@ public class GuideManager {
 
     public Thumbnail[] getThumbnails() {
         return thumbnails;
+    }
+
+    public void createGuide() {
+        boolean answer = true;
+        String title = "wow"; //TODO: Byt ut dessa mot riktiga värden från gui senare
+        String text = "woow";
+        byte[] image = {0};
+        currentGuide = new Guide();
+        while (answer) {
+            createCard(title, text, image);
+            answer = false;
+        }
+        cards = new Card[cardArrayList.size()][cardArrayList.size()];
+        for (int i = 0; i < cardArrayList.size(); i++) {
+            currentGuide.setCards(cards[0]);
+        }
+    }
+
+    public void createCard(String title, String text, byte[] image) {
+        Card newCard = new Card();
+        newCard.setTitle(title);
+        newCard.setText(text);
+        newCard.setImage(image);
+        cardArrayList.add(newCard);
     }
 
 
