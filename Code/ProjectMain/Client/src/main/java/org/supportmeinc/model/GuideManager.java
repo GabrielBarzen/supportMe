13e04b4192b549c2315d130ff1b990fb4939b51c
Raion1 <isak.holmqvist01@gmail.com>
2021-04-10 12:37:27 +0200

    Added core methods in Guide and down and uploading guides in GuideManager

--- Code/ProjectMain/Client/src/main/java/org/supportmeinc/model/GuideManager.java
+++ Code/ProjectMain/Client/src/main/java/org/supportmeinc/model/GuideManager.java
@@ -5,6 +5,9 @@ import shared.Guide;
 import shared.Thumbnail;
 import shared.User;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.*;
 import java.util.UUID;
 import java.util.ArrayList;
 import java.util.UUID;
@@ -43,6 +46,34 @@ public class GuideManager {
         Guide guide = connection.getGuide(id);
         return guide;
 
+    }
+
+    public void downloadGuide(Thumbnail thumbnail) {
+        Guide guide = getGuide(thumbnail);
+        try {
+            JFileChooser chooser = new JFileChooser();
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            chooser.showOpenDialog(null);
+            System.out.println(chooser.getCurrentDirectory());
+            ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(chooser.getSelectedFile() + "guides.dat")));
+            oos.writeObject(guides);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadGuides() {
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("DAT files" , "dat");
+        chooser.setFileFilter(filter);
+        chooser.showOpenDialog(null);
+        File file = chooser.getSelectedFile();
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)));
+            Guide guide = (Guide) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
